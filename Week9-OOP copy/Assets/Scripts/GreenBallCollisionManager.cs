b825acc4b5b2b73ed435d4fc5598b349801d3bc6
Larissa Heather <lvh@udel.edu>
2022-11-21 14:24:53 -0500

    UPDATED week 9
    added assets

---
+++ Week9-OOP copy/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public GameObject GreenBall;
+    public Vector3 firstPosition;
+    public float gap = 2;
+    public GameObject objectToCreate;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        Vector3 position = firstPosition;
+        for (int i = 0; i < 5; i++)
+        {
+
+            // Homework: Do something interesting here
+            Destroy(GreenBall);
+            Instantiate(objectToCreate, position, Quaternion.identity);
+            position.x += gap;
+        }
+    }
+}
+
+
+
---
+++ Week9-OOP copy/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public GameObject RedBall;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        Destroy(RedBall);
+        Instantiate(RedBall, transform.position, transform.rotation);
+
+    }
+}
+
+
+
+
+
